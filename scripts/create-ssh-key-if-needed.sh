f705503e7813fa08212ffff6ea0ff6ad6251d093
Andrew Reid East <andrew@andreweast.net>
2020-03-02 17:56:44 +0000

    Move ssh keys to a 'secrets' dir

--- scripts/create-ssh-key-if-needed.sh
+++ scripts/create-ssh-key-if-needed.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-if [[ ! -f id_rsa_vagrant ]]; then
+readonly SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+if [[ ! -f "${SCRIPT_DIR}/../secrets/id_rsa_vagrant" ]]; then
     echo "Creating an SSH keypair for Vagrant to use"
     # Generate a keypair:
-    #    - Filenames: id_rsa_vagrant id_rsa_vagrant.pub
+    #    - Filenames: id_rsa_vagrant & id_rsa_vagrant.pub
     #    - Comment in the .pub file of "vagrant@local"
     #    - No passphrase (-N '')
     #    - 2048 bits of entropy, which is more than adequate for local testing
-    ssh-keygen -t rsa -b 2048 -C "vagrant@local" -N '' -f ./id_rsa_vagrant
+    ssh-keygen -t rsa -b 2048 -C "vagrant@local" -N '' -f "${SCRIPT_DIR}/../secrets/id_rsa_vagrant"
 fi
